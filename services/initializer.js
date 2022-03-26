0decc94b6f090fbe039f4c36fbe3a824cb1dec38
Sam-Varghese <sam.varghese.sica@gmail.com>
2022-03-26 15:48:35 +0530

    Created initializer

---
+++ services/initializer.js
@@ -0,0 +1,37 @@
+import fs from "fs";
+import input from "input";
+import chalk from "chalk";
+
+const envFileName = "rough.env";
+
+export default async function InitializeApplication() {
+    console.clear();
+    return new Promise(async (resolve, reject) => {
+        const databaseName = await input.text(`Enter the database name: `, {
+            default: "PasswordManger",
+        });
+        const collectionName = await input.text(`Enter the collection name: `, {
+            default: "Passwords",
+        });
+        const masterPassword = await input.text(`Enter the master password: `);
+        const mongodbPort = await input.text(
+            `Enter the mongodb connection port number: `,
+            { default: "27017" }
+        );
+        fs.writeFile(
+            envFileName,
+            `MASTER_PASSWORD = "${masterPassword}"\nMONGODB_PORT = "mongodb://localhost:${mongodbPort}/"\nDATABASE_NAME = "${databaseName}"\nCOLLECTION_NAME: "${collectionName}"`,
+            (error) => {
+                if (error) {
+                    throw error;
+                    reject(`Failed to create .env file.`);
+                } else {
+                    console.log(chalk.green(`Made .env file...`));
+                    console.log(chalk.blue(`Initialized the application.`));
+                    resolve(`Database created.`);
+                }
+            }
+        );
+    });
+}
+InitializeApplication();
